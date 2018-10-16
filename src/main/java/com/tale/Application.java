b28e78dc4d03a9172d46b620011b1ad42908734c
SailHe <396103442@qq.com>
2018-10-16 17:34:43 +0800

    Heroku部署

--- src/main/java/com/tale/Application.java
+++ src/main/java/com/tale/Application.java
@@ -3,6 +3,9 @@ package com.tale;
 import com.blade.Blade;
 import com.blade.security.web.csrf.CsrfMiddleware;
 import com.tale.bootstrap.TaleLoader;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  * Tale启动类
@@ -14,7 +17,10 @@ public class Application {
     public static void main(String[] args) {
         Blade blade = Blade.of();
         TaleLoader.init(blade);
-        blade.use(new CsrfMiddleware()).start(Application.class, args);
+        String ePort = System.getenv("PORT");
+        blade.listen(Integer.valueOf(ePort == null ? "9000" : ePort))
+                .enableCors(true).devMode(false)
+                .use(new CsrfMiddleware()).start(Application.class, args);
     }
 
 }
